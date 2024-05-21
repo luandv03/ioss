5a31986f9bc7b848b6546c95124e399fca7b02c7
luandv03 <dinhvanluan2k3@gmail.com>
2024-05-21 21:33:24 +0700

    rebuild ItemSite entity

--- src/main/java/com/application/entity/ItemSite.java
+++ src/main/java/com/application/entity/ItemSite.java
@@ -3,10 +3,15 @@ package com.application.entity;
 // Mặt hàng trong hệ thống mà site đang kinh doanh.
 public class ItemSite extends Item {
     private int quantity;
+    private  String desiredDeliveryByShipDate;
+    private  String desiredDeliveryByAirDate;
 
-    public ItemSite(String itemId, String itemName, String unit, int quantity) {
+
+    public ItemSite(String itemId, String itemName, String unit, int quantity, String desiredDeliveryByShipDate, String desiredDeliveryByAirDate) {
         super(itemId, itemName, unit);
         this.quantity = quantity;
+        this.desiredDeliveryByShipDate = desiredDeliveryByShipDate;
+        this.desiredDeliveryByAirDate = desiredDeliveryByAirDate;
     }
 
     public int getQuantity() {
