10c52d1defb0827f267410ffe7c64b0b25881dc6
luandv03 <dinhvanluan2k3@gmail.com>
2024-06-12 12:12:11 +0700

    build us: inventory, creat orderlistitem, handle canceled order

---
+++ src/main/java/com/application/enums/OrderStatus.java
@@ -0,0 +1,16 @@
+package com.application.enums;
+
+public class OrderEnum {
+
+    public String getOrderStatus(String orderStatus) {
+        return switch (orderStatus) {
+            case "inActive" -> "Chưa gửi yêu cầu";
+            case "active" -> "Chờ xác nhận";
+            case "delivering" -> "Chờ giao";
+            case "done" -> "Đã nhận";
+            case "canceled" -> "Đã hủy";
+            case "deleted" -> "Đã xóa";
+            default -> "";
+        };
+    }
+}
